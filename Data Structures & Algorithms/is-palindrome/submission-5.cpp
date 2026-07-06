fc9b613c1a8176b98c086cdd4cd8d8562ee50ecc
orchidfriend <orchidfriend@gmail.com>
2026-07-07 01:30:55 +0800

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isAlphaN(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) return true;
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (!isAlphaN(s[i]) && (i < j)) i++;
+            while (!isAlphaN(s[j]) && (i < j )) j--;
+            if ((tolower(s[i]) == tolower(s[j]))) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
