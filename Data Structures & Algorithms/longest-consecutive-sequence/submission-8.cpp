c578ead29c2deb78d1d91b11e3e7d25587fbe265
orchidfriend <orchidfriend@gmail.com>
2026-07-10 01:23:21 +0800

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> streak;
+        int maxStreak = 0;
+
+        for (int a = 0; a < nums.size(); a++) {
+            if (!streak[nums[a]]) {
+                streak[nums[a]] = streak[nums[a] - 1] + streak[nums[a] + 1] + 1;
+                streak[nums[a] - streak[nums[a] - 1]] = streak[nums[a]];
+                streak[nums[a] + streak[nums[a] + 1]] = streak[nums[a]];
+                maxStreak = max(maxStreak, streak[nums[a]]);
+            }
+        }
+        return maxStreak;
+    }
+};
