e67bcfef87ef6d6f459c35644df6bd61b29f93cd
orchidfriend <orchidfriend@gmail.com>
2026-07-29 00:49:25 +0800

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> freq;
+        for (const auto& c : s1) {
+            freq[c]++;
+        }
+        unordered_map<char, int> count;
+        int must = freq.size();
+        int have = 0;
+        int i = 0;
+        for (int j = 0; j < s2.size(); j++) {
+            if (freq.find(s2[j]) != freq.end()) {
+                count[s2[j]]++;
+                if (count[s2[j]] == freq[s2[j]]) {
+                    have++;
+                    if (have == must) {
+                        return true;
+                    }
+                }
+                while (count[s2[j]] > freq[s2[j]]) {
+                    if (count[s2[i]] == freq[s2[i]]) have--;
+                    count[s2[i]]--;
+                    i++;
+                }
+            } else {
+                count.clear();
+                have = 0;
+                i = j + 1;
+            }
+        }
+        return false;
+    }
+};
