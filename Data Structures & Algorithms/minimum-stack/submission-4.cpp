22aabd62a2eccf8d2fec23b5da21f3aa6040e68e
orchidfriend <orchidfriend@gmail.com>
2026-08-06 01:33:22 +0800

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+   private:
+    int minVal;
+    stack<long> st;
+
+   public:
+    MinStack() {}
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push(0);
+            minVal = val;
+        } else {
+            st.push(val - minVal);
+            minVal = min(minVal, val);
+        }
+    }
+
+    void pop() {
+        if (st.empty()) return;
+        if (st.top() < 0) {
+            minVal = minVal - st.top();
+        }
+        st.pop();
+    }
+
+    int top() {
+        if (st.top() < 0)
+            return minVal;
+        else
+            return st.top() + minVal;
+    }
+
+    int getMin() { return minVal; }
+};
