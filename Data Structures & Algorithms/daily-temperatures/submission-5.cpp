974785a5c0da9bf6d37d3f1227990dd9961202a4
orchidfriend <orchidfriend@gmail.com>
2026-08-16 01:11:30 +0800

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        int high = temperatures.size() - 1;
+        for (int a = temperatures.size() - 2; a >= 0; a--) {
+            if (temperatures[high] > temperatures[a]) {
+                result[a] = high - a;
+            } else {
+                while (temperatures[high] <= temperatures[a] && (result[high] != 0)) {
+                    high += result[high];
+                }
+                if (temperatures[high] > temperatures[a]) {
+                    result[a] = high - a;
+                }
+            }
+            high = a;
+        }
+        return result;
+    }
+};
