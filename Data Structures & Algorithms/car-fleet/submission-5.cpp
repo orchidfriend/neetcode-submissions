9bd29bdac978f8462f587c57a7f835356cd50a23
orchidfriend <orchidfriend@gmail.com>
2026-09-10 01:13:17 +0800

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+        for (int a = 0; a < position.size(); a++) {
+            cars.push_back({position[a], speed[a]});
+        }
+        sort(cars.begin(), cars.end(), std::greater<pair<int, int>>());
+        stack<float> times;
+        for (const auto& car : cars) {
+            float time = (float)(target - car.first) / car.second;
+            if (times.empty() || times.top() < time) {
+                times.push(time);
+            }
+        }
+        return times.size();
+    }
+};
